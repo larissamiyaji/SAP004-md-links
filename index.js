e58b36006d4ad316e8b0088065354125ca214e0e
larissamiyaji <natsumi-23@hotmail.com>
2020-07-03 16:45:47 -0300

    Declaração da variáel com o REGEX

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/*
 module.exports = () => {
   // ...
 };
+*/
+const fs = require("fs");
+//  const file = process.argv[2];
+//  function mdLinks
+
+
+// Regex para Markdown
+const regex = /\[([^\]]*)\]\(([^\)]*)\)/.mg;
