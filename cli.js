8cc23e43ba8bc4804ff057c161845013aae0472b
larissamiyaji <natsumi-23@hotmail.com>
2020-07-03 16:46:15 -0300

    Teste com o chalk

--- cli.js
+++ cli.js
@@ -3,7 +3,24 @@
 //  Grab provided args.
 const [, , ...args] = process.argv;
 const http = require("https");
-
+const chalk = require('chalk');
 //  Print Hello World provided args.
-console.log(`Hello World ${args}`);
+console.log(chalk.magenta(`Oi, Deus. Sou eu de novo ${chalk.yellow(args)}`));
 
+
+//          Cores do Chalk
+//  console.log(chalk.red("Hello"));
+//  console.log(chalk.green("Hello"));
+//  console.log(chalk.yellow("Hello"));
+//  console.log(chalk.blue("Hello"));
+//  console.log(chalk.magenta("Hello"));
+//  console.log(chalk.cyan("Hello"));
+//  console.log(chalk.white("Hello"));
+//  console.log(chalk.gray("Hello"));
+//  console.log(chalk.bgRed("Hello"));
+//  console.log(chalk.bgGreen("Hello"));
+//  console.log(chalk.bgYellow("Hello"));
+//  console.log(chalk.bgBlue("Hello"));
+//  console.log(chalk.bgMagenta("Hello"));
+//  console.log(chalk.bgCyan("Hello"));
+//  console.log(chalk.bgWhite("Hello"));
