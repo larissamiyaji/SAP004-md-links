1c78a598c92bf483f5c74fdaa4a1ed80d130eb16
larissamiyaji <natsumi-23@hotmail.com>
2020-07-03 18:08:20 -0300

    Inclusão do regex

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ module.exports = () => {
   // ...
 };
 */
-const fs = require("fs");
-//  const file = process.argv[2];
 //  function mdLinks
 
+//  Regex para Markdown
+//  const regex = /\[([^\]]*)\]\(([^\)]*)\)/mg;
 
-// Regex para Markdown
-const regex = /\[([^\]]*)\]\(([^\)]*)\)/.mg;
+const fs = require("fs"); //  Biblioteca para fazer a transmissão do doc no Node
+const files = []; //  Guardar temporariamente os arquivos.
+const fileMD = process.argv[2]; //  Pega o arquivo na posição 2 da CLI
+const regex = /(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/gm; //  Regex para Markdown
