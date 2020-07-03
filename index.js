03ae64091bcd6ca9882ed987e424b5ca2962939f
larissamiyaji <natsumi-23@hotmail.com>
2020-07-03 20:15:55 -0300

    Criação inicial da função mdLinks (continuar com a função de map dos links)

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-/*
-module.exports = () => {
-  // ...
-};
-*/
-//  function mdLinks
-
-//  Regex para Markdown
-//  const regex = /\[([^\]]*)\]\(([^\)]*)\)/mg;
-
 const fs = require("fs"); //  Biblioteca para fazer a transmissão do doc no Node
-const files = []; //  Guardar temporariamente os arquivos.
+
+//  const regex = /\[([^\[\]]+)\]\((https?:\/\/[^\)]*)\)/gm; //  Regex para Markdown
+//  const files = []; //  Guardar temporariamente os arquivos.
 const fileMD = process.argv[2]; //  Pega o arquivo na posição 2 da CLI
-const regex = /\[([^\[\]]+)\]\((https?:\/\/[^\)]*)\)/gm; //  Regex para Markdown
+
+function mdLinks(file){
+  return new Promise((resolved, rejected) => {
+    fs.readFile(file, "utf-8", (err, data) => {
+      if(err){
+        rejected(err.message);
+      } else {
+        const regex = data.match(/\[([^\[\]]+)\]\((https?:\/\/[^\)]*)\)/gm);
+        const 
+      }
+    })
+  })
+}
+
+module.exports = mdLinks;
