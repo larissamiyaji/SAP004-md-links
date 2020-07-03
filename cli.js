b4a035bb1de1f3551ada89cd020b33bcfb0e01f8
larissamiyaji <natsumi-23@hotmail.com>
2020-07-02 21:39:47 -0300

    Reinicialização do package.json

--- cli.js
+++ cli.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 //  Grab provided args.
-const [,, ...args] = process.argv
+const [, , ...args] = process.argv;
+const http = require("https");
+
 
 //  Print Hello World provided args.
-console.log(`Hello World ${args}`)
+console.log(`Hello World ${args}`);
