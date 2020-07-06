f9eb8cc7849f677990bec30fca7663aec334c535
larissamiyaji <natsumi-23@hotmail.com>
2020-07-06 19:15:19 -0300

    Reinicialização da função mdLinks e agora consigo printar o README.md no terminal

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-const fs = require("fs"); //  Biblioteca para fazer a transmissão do doc no Node
+//  Este arquivo deve exportar a função mdLinks que será chamada na cli.js 👌
+const fs = require("fs");
 
-//  const regex = /\[([^\[\]]+)\]\((https?:\/\/[^\)]*)\)/gm; //  Regex para Markdown
-//  const files = []; //  Guardar temporariamente os arquivos.
-const fileMD = process.argv[2]; //  Pega o arquivo na posição 2 da CLI
-
-function mdLinks(file){
+function mdLinks(file) {
   return new Promise((resolved, rejected) => {
     fs.readFile(file, "utf-8", (err, data) => {
-      if(err){
+      //  "data" é o conteúdo dentro do arquivo
+      console.log(file); // Lê a 1ª linha do documento index.js e diz se é um diretório (pelo menos isso, Senhor 🙌) [[[Pensar em como fazer ler o resto]]]
+      console.log(data); // Lê todo o conteúdo do README.md com o comando "node index.js README.md"
+      console.log(err);
+      if (err) {
         rejected(err.message);
       } else {
         const regex = data.match(/\[([^\[\]]+)\]\((https?:\/\/[^\)]*)\)/gm);
-        const mapText = regex.map //  Continuar função de mapeamento dos links
+        const mapText = regex.map((item) => {
+          const splitText = item.split("](");
+          const text = splitText[0].replace("\n", "");
+          const href = splitText[1];
+          return { text, href, file };
+        });
+        resolved(mapText);
       }
-    })
-  })
+    });
+  });
 }
 
+const filePosition = process.argv[2]; //  Pega o arquivo na posição 2 da CLI
+mdLinks(filePosition); //  Mostra o README.md no terminal
+
 module.exports = mdLinks;
+
+//  const regex = /\[([^\[\]]+)\]\((https?:\/\/[^\)]*)\)/gm; // Regex para Markdown
+//  const file = []; //  Guardar temporariamente os arquivos.
