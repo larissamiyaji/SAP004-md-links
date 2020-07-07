ce67dfa343ef30e14eeb1256855e2d90357e97ea
larissamiyaji <natsumi-23@hotmail.com>
2020-07-06 21:46:57 -0300

    Organização do documento

--- cli.js
+++ cli.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 //  Esse documento irá chamar e manipular a função mdLinks criada no index.js 👌
 
-
 const chalk = require("chalk"); //  Pra deixar o console mais colorido e eu ficar feliz 😃
-const [, , ...args] = process.argv; //  Pega os argumentos passados
-//  const http = require("https"); //   Isso eu não lembro porque coloquei aqui... 😒
-//  const mdLinks = require("./index.js"); //   Importando a função mdLinks do index.js 👌
+const mdLinks = require("./index.js"); //   Importando a função mdLinks do index.js 👌
 
-console.log(chalk.cyan(`Oi, Deus. Sou eu de novo ${chalk.magenta(args)}`)); //  Printa "Hello World" + os argumentos dados
+const filePosition = process.argv[2]; //  Pega o arquivo na posição 2 da CLI
+mdLinks(filePosition).then().catch();
 
-//  mdLinks.then().catch(); //  Fazer a promise
+//  const http = require("https"); //   Conseguir ler os links com https
+//  console.log(chalk.cyan(`Oi, Deus. Sou eu de novo ${chalk.magenta(args)}`)); //  Printa "Hello World" + os argumentos dados
+//  const [, , ...args] = process.argv; //  Pega os argumentos passados
