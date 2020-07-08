e9f026fd757b80320f0093ddafd4be1ed1c85c53
larissamiyaji <natsumi-23@hotmail.com>
2020-07-08 13:58:37 -0300

    Correção do layout dos links no terminal e separação com uma gambiarrazinha

--- cli.js
+++ cli.js
@@ -5,18 +5,28 @@ const chalk = require("chalk");
 const mdLinks = require("./index.js");
 const filePosition = process.argv[2]; //  Pega o arquivo na posição 2 da CLI
 
+let linksEmptyArray = [];
 const links = function () {
   mdLinks(filePosition)
     .then((mapText) => {
+        console.log(mapText); //  Retorna o array de objetos do mapText
       mapText.forEach((element) => {
-        console.log(
+        let linksArray = linksEmptyArray.push(/*"Testando")*/ //  Aparece números muito doidos
+          console.log(chalk.green.bold(`Nome: ${element.text}`)),
+          console.log(chalk.yellow.bold(`Link: ${element.href}`)),
+          console.log(chalk.grey.bold(`Documento: ${element.file}`)));
+          console.log("----------"); // Deixar separado cada link
+        //  console.log(linksArray); // Aparece números muito doidos
+        //  console.log(linksEmptyArray); //  Aparece como "undefined"
+
+        /*console.log(
           chalk.green.bold(`Nome: ${element.text}`),
           chalk.yellow.bold(`Link: ${element.href}`),
           chalk.grey.bold(`Documento: ${element.file}`)
-        );
+        );*/
       });
     })
-    .catch(console.log("Houve algum erro"));
+    .catch(console.log("Erro"));
 };
 
 links(filePosition);
