9bdda6055ac638550b52dc823f7c7f9063762796
larissamiyaji <natsumi-23@hotmail.com>
2020-07-08 15:52:58 -0300

    Finalização da função mdLinks. Iniciar a função do validate

--- cli.js
+++ cli.js
@@ -9,16 +9,20 @@ let linksEmptyArray = [];
 const links = function () {
   mdLinks(filePosition)
     .then((mapText) => {
-       // console.log(mapText); //  Retorna o array de objetos do mapText
+      // console.log(mapText); //  Retorna o array de objetos do mapText
       mapText.forEach((element) => {
         let linksArray = linksEmptyArray.push(
           console.log(chalk.green.bold(`Nome: ${element.text}`)),
           console.log(chalk.yellow.bold(`Link: ${element.href}`)),
-          console.log(chalk.grey.bold(`Documento: ${element.file}`)));
-          console.log("----------"); // Deixar separado cada link
+          console.log(chalk.grey.bold(`Documento: ${element.file}`))
+        );
+        console.log("----------"); // Deixar separado cada link
       });
     })
     .catch(console.log("Erro"));
 };
 
 links(filePosition);
+
+//Função Validate (verifica se o link funciona ou não)
+const axios = require("axios");
